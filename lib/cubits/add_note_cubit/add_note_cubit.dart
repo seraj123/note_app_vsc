89a2336c315e3fe7dfffc07977071b1e03f41019
seraj123 <sisoo.seraj.com>
2025-12-17 04:13:42 +0300

    refrash note list

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -3,18 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/model/note_model.dart';
 import 'package:hive/hive.dart';
 
-import '../constance.dart';
+import '../../constance.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-  void addNote(NoteModel note) async {
+  void addNote(NoteModel note) async{
     emit(AddNoteLoading());
     try {
       var noteBox = Hive.box<NoteModel>(kNoteBox);
-      emit(AddNoteSuccess());
-      await noteBox.add(note);
+
+         await noteBox.add(note);
+         emit(AddNoteSuccess());
+
     } on Exception catch (e) {
       emit(AddNoteFailure(e.toString()));
     }
